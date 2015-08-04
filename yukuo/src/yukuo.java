a1fd27ac2b6783e49ca017d2ad250d0cfcb14aeb
許煜國 <104021010@live.asia.edu.tw>
2015-08-04 10:44:37 +0800

    v1.1
    java

---
+++ yukuo/src/yukuo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class yukuo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn=new Scanner(System.in);
+        System.out.print("請輸入攝氏度");
+        float c=scn.nextFloat();
+        float f=c*9/5+32;
+        System.out.println("華氏度為"+f);
+       
+	} 
+
+}
