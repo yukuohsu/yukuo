1f2003cb62d5f2c135858620b8dccd2220b6d7cc
許煜國 <104021010@live.asia.edu.tw>
2015-08-05 10:40:08 +0800

    Update 20150803n2.java

--- yukuo/src/20150803n2.java
+++ yukuo/src/20150803n2.java
@@ -5,10 +5,10 @@ public class yukuo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Scanner scn=new Scanner(System.in);
-        System.out.print("½Ğ¿é¤JÄá¤ó«×");
+        System.out.print("è«‹è¼¸å…¥æ”æ°åº¦");
         float c=scn.nextFloat();
         float f=c*9/5+32;
-        System.out.println("µØ¤ó«×¬°"+f);
+        System.out.println("è¯æ°åº¦ç‚º"+f);
        
 	} 
 
