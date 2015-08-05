debadc6835996cada6bce763d70b6fdb841c15f7
許煜國 <104021010@live.asia.edu.tw>
2015-08-05 14:22:33 +0800

    v1.2
    java 判斷一個正整數為奇數或偶數

---
+++ yukuo/src/20150805n7.java
@@ -0,0 +1,18 @@
+package y20150805;
+import java.util.Scanner;
+public class y20150805n7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        System.out.println("叫块タ俱计");
+        int a = scn.nextInt();
+        if(a%2==0){
+        System.out.println(a+"案计");
+	}else{
+		System.out.println(a+"计");
+	}
+        
+	}
+
+}
