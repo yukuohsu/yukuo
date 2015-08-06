96a72c53635f979e8ffe1d63f71fccc35570c92f
許煜國 <104021010@live.asia.edu.tw>
2015-08-06 15:01:49 +0800

    v1.3
    for loop 13~1399奇數和

---
+++ yukuo/src/20150806n4.java
@@ -0,0 +1,21 @@
+package y20150806;
+import java.util.Scanner;
+public class y20150806n4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤J13");
+		int a = scn.nextInt();
+		int i;
+		int sum=0;
+			
+		for(i=a;i<=1399;i=i+2){
+				sum=sum+i;
+			}
+				System.out.println("sum="+sum);
+			
+			}
+		}
+
+	
