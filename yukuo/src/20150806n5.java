d0801c37c4887aadc2ffa88fc440c6571514e67a
許煜國 <104021010@live.asia.edu.tw>
2015-08-06 15:16:55 +0800

    v1.3
    for loop 13~1399能被17整除的數的總和

---
+++ yukuo/src/20150806n5.java
@@ -0,0 +1,22 @@
+package y20150806;
+import java.util.Scanner;
+public class y20150806n5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤J13");
+		int a = scn.nextInt();
+		int i;
+		int sum=0;
+			for(i=17;i<=1399;i=i+17){
+			sum=sum+i;
+			}
+			System.out.println("sum="+sum);
+		
+
+	}
+}
+	
+	
+	
