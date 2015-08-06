5caa575f76bb92278648f8531c2b4fd607907000
許煜國 <104021010@live.asia.edu.tw>
2015-08-06 14:40:30 +0800

    v1.3
    for loop 13加到1399

---
+++ yukuo/src/20150806n3.java
@@ -0,0 +1,19 @@
+package y20150806;
+import java.util.Scanner;
+public class y20150806n3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入一個值");
+		int n = scn.nextInt();
+		int i ;
+		int sum = 0;
+		for(i=n;i>=13 && i<=1399;i=i+1){
+           sum=sum+i;
+		}
+           System.out.println("sum="+sum);
+
+	}
+
+}
