5e94a7e4c02eee3442e8dc9c9d6d7f16beae978e
許煜國 <104021010@live.asia.edu.tw>
2015-08-10 10:53:26 +0800

    v1.4
    for loop (輸入一個數  *遞增)

---
+++ yukuo/src/20150810n1.java
@@ -0,0 +1,29 @@
+package y20150810;
+import java.util.Scanner;
+public class y20150810n1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("¿é¤Jn");
+		int n = scn.nextInt();
+		int i ;
+		int j ;
+		int k=0 ;
+		int sum=0;
+		for(i=0;i<n;i++){
+		System.out.println();
+		for(j=0;j<=n-i-1;j++){
+		System.out.print(" ");
+		}
+		for(k=0;k<=2*i;k++){
+			System.out.print("*");
+		}
+		
+		
+	}
+		
+	}
+	}
+
+
