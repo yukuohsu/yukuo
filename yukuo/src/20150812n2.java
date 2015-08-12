12a1a541a27f98b51d6978f06a40d1f48e311e40
許煜國 <104021010@live.asia.edu.tw>
2015-08-12 11:09:57 +0800

    v1.5
    for loop  (n!)

---
+++ yukuo/src/20150812n2.java
@@ -0,0 +1,19 @@
+package y20150812;
+import java.util.Scanner;
+public class y20150812n2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入一正整數");
+		int a = scn.nextInt();
+		int i;
+		int sum=1;
+		for(i=1;i<=a;i++){
+		sum=sum*i;
+		}
+		System.out.println(a+"!="+sum);
+
+	}
+
+}
