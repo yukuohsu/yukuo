d03d83338fd21a06687338d120d7cf2f61a57acb
許煜國 <104021010@live.asia.edu.tw>
2015-08-12 11:15:49 +0800

    v1.5
    while loop (n!)

---
+++ yukuo/src/20150812n3.java
@@ -0,0 +1,20 @@
+package y20150812;
+import java.util.Scanner;
+public class y20150812n3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入一正整數");
+		int a = scn.nextInt();
+		int i = 1;
+		int sum = 1;
+		while(i<=a){
+		sum=sum*i;
+		i++;
+		}
+		System.out.println(a+"!="+sum);
+
+	}
+
+}
