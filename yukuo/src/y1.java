90526442bdb56579fc99628e7c637fac056ed542
許煜國 <104021010@live.asia.edu.tw>
2015-08-21 11:49:18 +0800

    Specialtopic
    v1.1

---
+++ yukuo/src/y1.java
@@ -0,0 +1,19 @@
+package Specialtopic;
+
+import java.awt.*;
+
+import y20150820.swing;
+
+public class y1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		start mFrm= new start();
+		mFrm.setVisible(true);
+		mFrm.setSize(1200,900);
+		mFrm.setLocation(10,0);
+		mFrm.setTitle("Mouse Event");
+		//mFrm.addMouseMotionListener(mFrm);
+	}
+
+}
